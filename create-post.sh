b6fa069a8f06d8ec70e156d64b6aae86f3bf9c54
hectoraldairah <hectoraldairah@protonmail.com>
2022-07-24 18:47:05 -0500

    Create Post v0.0.1

---
+++ create-post.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+format_tags(){
+ for tag in "$@"; do
+  echo "$tag"  
+ done
+}
+
+cd ~/Proyects/bitbyte-blog/src/posts
+
+read -p "Post Name: " post_name
+
+file_name="$(
+  echo $post_name | 
+  awk '{print tolower($0)}' | 
+  sed 's/ /_/g').md"
+
+read -p "Description : " post_desc
+read -p "Featured post?: (y/n) " -n 1 featured
+echo ""
+read -p "Tags: " -a tags
+
+pos=1
+tag_length=${#tags[@]}
+for tag in "${tags[@]}"; do
+  if [ "$pos" -eq "$tag_length" ]; then
+    line_tag+="'${tag}'"
+  else
+    line_tag+="'${tag}',"
+  fi
+    ((pos += 1))
+done
+
+cat > $file_name <<EOL
+---
+title: $post_name
+date: $(date +%Y-%m-%d)
+description: $post_desc
+featured: $([ "$featured" == "y" ] && echo "true" || echo "false")
+tags: [$line_tag]
+---
+
+# Header 1 
+
+## Header 2 
+
+### Header 3 
+
+EOL
+
+nvim "$file_name"
